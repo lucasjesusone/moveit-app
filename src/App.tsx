5b4f27651359712e2b1c4a8c2693b00e2722d56e
lucasjesusone <lucasjesusone@gmail.com>
2021-02-23 00:05:21 -0400

    #1 Cleaning code

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react'
+
+
+
 
 function App() {
+  const [ counter, SetCounter] = useState(1)
+
+  function increment() {
+  SetCounter(counter + 1 )
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <button
+   onClick={increment}
+   style={{backgroundColor: 'blue'}}
+   >
+     Incremente
+     <span>{counter}</span>
+   </button> 
   );
 }
 
