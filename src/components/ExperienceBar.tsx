a8be5442d166f1f2971bba5b5eae22f6f50dc443
lucasjesusone <lucasjesusone@gmail.com>
2021-02-24 01:15:49 -0400

    Alteração espaçamento na barra de XP

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from '../styles/components/ExperienceBar.module.css'
+
+function ExperienceBar() {
+
+
+
+
+    return (
+        <header className={styles.experienceBar}>
+            <span>0 xp</span>
+            <div>
+                <div style={{width: '50%'}} />          { /*Cor da barra */  }
+
+                <span className={styles.currentExperience} style={{left: '50%'}}>300 xp</span>
+            </div>
+            
+            <span>600 xp</span>
+        </header>
+    )
+}
+
+export default ExperienceBar
