bbcc87c611a145cbf5b482fdb626de891d99646e
lucasjesusone <lucasjesusone@gmail.com>
2021-02-24 01:16:03 -0400

    Criação do Perfil com img

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/LucasJesusone.png" alt="Lucas Gabriel"></img>
+
+        <div>
+            <strong>Lucas Gabriel</strong>
+            <p>
+                <img src="icons/level.svg" alt="Level"/>
+                Level 1
+            </p>
+        </div>
+
+        </div>
+    );
+}
