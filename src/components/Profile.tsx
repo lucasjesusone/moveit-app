5e5c90c35aa62d7d8ff42f5acc7849c93d2d8d8d
lucasjesusone <lucasjesusone@gmail.com>
2021-02-27 13:30:19 -0400

    Alterações de valores fixos Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/LucasJesusone.png" alt="Lucas Gabriel"></img>
@@ -9,7 +13,7 @@ export function Profile() {
         <strong>Lucas Gabriel</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
