7919bf648fb0bce38d9b196c510dae5f4668e461
lucasjesusone <lucasjesusone@gmail.com>
2021-02-27 23:26:37 -0400

    Criando a tela de Login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import React from "react";
+import styles from "../styles/pages/Login.module.css";
+
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Login</title>
+      </Head>
+
+      <section>
+        <div>
+          <span>
+            <img src="/icons/logotime.svg" alt="Logo time-action" />
+            <h1>Move.it</h1>
+          </span>
+          <h2>Faça seu login na plataforma</h2>
+        </div>
+
+        <div>
+          <h3>Bem-vindo</h3>
+
+          <span>
+            <p>
+              <img src="/icons/github.svg" alt="github logo" />
+              Acesse com seu Github para começar
+            </p>
+          </span>
+
+          <form>
+            <input
+              type="username"
+              name="username"
+              placeholder="Digite seu username"
+            />
+
+            <button type="button">Login</button>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+}
