797b0812321c6f8580ddd902970fcbb9daeef584
lucasjesusone <lucasjesusone@gmail.com>
2021-03-16 00:58:46 -0400

    feat: creating api to auth

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,28 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers'
+
+
+// Install dependencies
+
+const options = {
+    providers: [
+        Providers.Github({
+            clientId: process.env.
+            clientSecret: process.env
+            authorizationUrl: ""
+        }),,
+
+    ]
+    session: {
+        jwt: true,
+    },
+
+    jwt : {
+        secret: process.env
+    }
+}
+
+export default (req, res) => NextAuth(req, res, options)
+
+
+// Finish to config .env
